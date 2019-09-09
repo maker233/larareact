4312fee3d6b3ca180a65fbf864a8ad5b9eb7a3ad
Javier Pérez-Chao <javierperezchao@Javiers-Mac-mini.local>
2019-09-09 18:25:18 +0200

    first commit

---
+++ taskman/resources/js/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={ProjectsList} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
