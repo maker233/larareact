b0632c9c70f5929f42ba59850d118eb31589e7b0
Javier Pérez-Chao <javierperezchao@Javiers-Mac-mini.local>
2019-09-10 15:23:17 +0200

    task manager finished

--- taskman/resources/js/components/App.js
+++ taskman/resources/js/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./Header";
-
+import NewProject from "./NewProject";
+import ProjectsList from "./ProjectsList";
+import SingleProject from "./SingleProject";
 class App extends Component {
     render() {
         return (
@@ -11,9 +13,12 @@ class App extends Component {
                     <Header />
                     <Switch>
                         <Route exact path="/" component={ProjectsList} />
+                        <Route path="/create" component={NewProject} />
+                        <Route path="/:id" component={SingleProject} />
                     </Switch>
                 </div>
             </BrowserRouter>
+            
         );
     }
 }
